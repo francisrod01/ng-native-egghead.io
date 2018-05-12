af8184b7b22e50030b1694866cb485553ff83f34
Francis Rodrigues <francis.rod01@outlook.com>
2018-05-11 21:35:15 -0300

    Handle back navigation when using page-router-outlet

--- app/components/details/details.component.ts
+++ app/components/details/details.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {RouterExtensions} from 'nativescript-angular/router';
 
 
 @Component({
@@ -7,4 +8,10 @@ import {Component} from '@angular/core';
   templateUrl: './details.component.html'
 })
 
-export class DetailsComponent {}
+export class DetailsComponent {
+  constructor(private router: RouterExtensions) { }
+
+  public goBack(): void {
+    this.router.back();
+  }
+}
