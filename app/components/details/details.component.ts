82aead6b609cd03a53175ecb62efd787b1760b0e
Francis Rodrigues <francis.rod01@outlook.com>
2018-05-28 20:57:32 -0300

    Display lists using NativeScript's ListView component

--- app/components/details/details.component.ts
+++ app/components/details/details.component.ts
@@ -10,8 +10,31 @@ import {ModalDialogParams} from 'nativescript-angular/directives/dialogs';
 })
 
 export class DetailsComponent {
+  public items: any[] = [
+    { title: 'NativeScript' },
+    { title: 'Angular' },
+    { title: 'TypeScript' },
+    { title: 'JavaScript' }
+  ];
+
   constructor(private router: RouterExtensions, private params: ModalDialogParams) {
-    console.log('params: ', params.context.msg);
+    console.log('constructor params: ', params.context.msg);
+  }
+
+  public onLoaded(e) {
+    console.log('==== onLoaded(e) loaded.', e);
+  }
+
+  public onItemLoading(e) {
+    console.log('=== onItemLoading(e) loaded.', e);
+  }
+
+  public onItemTap(e) {
+    console.dir('==== onItemTap(e) loaded.', e);
+
+    for (let key in e) {
+      console.log('onItemTap(e) loaded.', `${key}: ${e[key]}`);
+    }
   }
 
   public goBack(): void {
